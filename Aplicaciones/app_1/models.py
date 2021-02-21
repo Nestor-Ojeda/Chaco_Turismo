fc57da1918a154773540fcc0d9317f26a286b860
[fabi] <[nestorfabi678@gmail.com]>
2021-02-21 01:57:08 -0300

    class provincia y ciudad

--- Aplicaciones/app_1/models.py
+++ Aplicaciones/app_1/models.py
@@ -9,6 +9,16 @@ class Paises(models.Model):
 		codigo = models.CharField(max_length = 2)
 		pais = models.CharField(max_length=100)
 
+class Provincia(models.Model):
+	nombre = models.CharField(max_length=100)
+
+class Ciudad(models.Model):
+	cod_Postal = models.CharField(max_length=10)
+	nom_ciudad = models.CharField(max_length=100)
+	zona_turismo = models.CharField(max_length=100)
+
+
+
 
 
 
